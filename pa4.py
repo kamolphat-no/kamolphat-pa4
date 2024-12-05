5dc119ecd445797b44b844ede607a07274b6920c
kamolphat-no <kamolphat.no@gmail.com>
2024-12-05 22:24:27 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -9,15 +9,15 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """ Act as a song analyst. You will receive a song name and full lyrics 
             and you will give the song key message, theme, interesting words that support this theme
-            and other songs related with this theme. Give me a list of key message and theme in JSON string.
-            And List 10 intersting words in JSON array, one word per line.
+            and other songs related with this theme. Give me a list of key message and theme in dictionary.
+            And List 10 intersting words in dictionary, one word per line.
             Each word should have 3 fields:
             - "word" - word that support song theme or key message if word is not in english, 
                        give that word in original language
             - "definition" - if the word is in english, give the definition in english 
                              but if the word is in other langauges, give the english translation
             - "description" - a description why that word support the theme
-            And give me a list of other recommended songs in JSON string.
+            And give me a list of other recommended songs in dictionary.
             The key message should be 1 to 2 sentences only. The theme should be only 3 words.
             Other related songs should be 3 to 5 songs only.
             Don't say anything at first. Wait for the user to say something.
@@ -45,11 +45,9 @@ if st.button('Send'):
     st.subheader('**AI response :**')
     ai_response = response.choices[0].message.content
 
-    response_dict = json.loads(ai_response)
-
-    key_message = response_dict['key_message']
+    key_message = ai_response['key_message']
     st.write('**Key Message** \n')
     st.write(key_message)
-    
-    st.write(response_dict)
+
+    st.write(ai_response)
 
