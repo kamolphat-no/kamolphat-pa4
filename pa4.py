4a104faab652bed786d9fac8508aadcefd6d461b
kamolphat-no <kamolphat.no@gmail.com>
2024-12-05 23:38:24 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import openai
+import orjson
 import json
 import pandas as pd
 
@@ -45,7 +46,7 @@ if st.button('Send'):
     st.subheader('**AI response :**')
     ai_response = response.choices[0].message.content
 
-    response_dict = json.loads(ai_response)
+    response_dict = orjson.loads(ai_response)
 
     key_message = response_dict['key_message']
     st.write('**Key Message**')
