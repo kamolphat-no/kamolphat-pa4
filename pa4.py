0742bf1564b13ea71e8fe95df4cdc818844d9e1d
kamolphat-no <kamolphat.no@gmail.com>
2024-12-05 21:54:47 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -9,7 +9,7 @@ user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 client = openai.OpenAI(api_key=user_api_key)
 prompt = """ Act as a song analyst. You will receive a song name and full lyrics 
             and you will give the song key message, theme, interesting words that support this theme
-            and other songs related with this theme. Give me a list of key message and theme.
+            and other songs related with this theme. Give me a list of key message and theme in JSON string.
             List 10 intersting words in JSON array, one word per line.
             Each word should have 3 fields:
             - "word" - word that support song theme or key message if word is not in english, 
@@ -17,7 +17,7 @@ prompt = """ Act as a song analyst. You will receive a song name and full lyrics
             - "definition" - if the word is in english, give the definition in english 
                              but if the word is in other langauges, give the english translation
             - "description" - a description why that word support the theme
-            And give me a list of other recommended songs.
+            And give me a list of other recommended songs in JSON string.
             The key message should be 1 to 2 sentences only. The theme should be only 3 words.
             Other related songs should be 3 to 5 songs only.
             Don't say anything at first. Wait for the user to say something.
