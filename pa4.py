cedc408f859efeae73824a953d863f82c79a2c82
kamolphat-no <kamolphat.no@gmail.com>
2024-12-07 14:49:51 +0700

    Add files via upload

--- pa4.py
+++ pa4.py
@@ -7,20 +7,23 @@ import pandas as pd
 user_api_key = st.sidebar.text_input("OpenAI API key", type="password")
 
 client = openai.OpenAI(api_key=user_api_key)
-prompt = """ Act as a song analyst. You will receive a song name and full lyrics 
+prompt = """ Act as a song analyst. You will receive full lyrics of the song 
             and you will give the song key message, theme, interesting words that support this theme
-            and other songs related with this theme. Give me a list of key message and theme in JSON array.
+            and other songs related with this theme. 
+            Give me a list of key message in 1 to 2 sentences and theme in 3 words in JSON array
             And List 10 intersting words in JSON array, one word per line.
             Each word should have 3 fields:
-            - "Word" - word that support song theme or key message if word is not in english, 
+            - "word" - word in lyrics that support song theme or key message. If word is not in english, 
                        give that word in original language
-            - "Definition" - if the word is in english, give the definition in english 
-                             but if the word is in other langauges, give the english translation
-            - "Description" - a description why that word support the theme
-            And give me a list of other recommended songs in JSON array.
-            All of the response should be in JSON array.
-            The key message should be 1 to 2 sentences only. The theme should be only 3 words.
-            Other related songs should be 3 to 5 songs only.
+            - "definition" - definition if that word. If the word is in english, give the definition in english.
+                             but if the word is in other langauges, give the english translation.
+            - "description" - a description why that word support the theme
+            And give me a list of 5 other recommended songs in JSON array.
+            Give response in JSON array as this structure :
+            [ {'key_message' : ' ' }  , {'theme' : ' ' } , 
+            {'interesting_words' : [{'word' : ' ' , 'definition' : ' ' , 'description' : ' '} , ..... ,
+            {'word' : ' ' , 'definition' : ' ' , 'description' : ' '}] } ,
+            {'related_songs' : [' ' , ' ' , ' ' , ' ' , ' ' ]} ]
             Don't say anything at first. Wait for the user to say something.
         """
 
@@ -49,27 +52,26 @@ if st.button('Send'):
     st.write(ai_response)
     st.write(type(ai_response))
 
-
     response_dict = json.loads(ai_response)
 
     st.write(response_dict)
 
-    key_message = response_dict.get('key_message', [])
-    st.subheader('Key Message')
-    st.write(key_message)
+    # key_message = response_dict.get('key_message', [])
+    # st.subheader('Key Message')
+    # st.write(key_message)
 
-    theme = response_dict.get('theme', [])
-    st.subheader('\n Theme')
-    st.write(theme)
+    # theme = response_dict.get('theme', [])
+    # st.subheader('\n Theme')
+    # st.write(theme)
 
-    vocab_df = pd.DataFrame.from_dict(response_dict.get('interesting_words', []))
-    st.subheader('\n Interesting word list')
-    st.table(vocab_df)
+    # vocab_df = pd.DataFrame.from_dict(response_dict.get('interesting_words', []))
+    # st.subheader('\n Interesting word list')
+    # st.table(vocab_df)
 
-    other_songs = response_dict.get('related_songs', [])
-    st.subheader('\n Other recommended songs')
-    for index, song in enumerate(other_songs) :
-        num = index + 1
-        st.write(f'{num}. {song}')
+    # other_songs = response_dict.get('related_songs', [])
+    # st.subheader('\n Other recommended songs')
+    # for index, song in enumerate(other_songs) :
+    #     num = index + 1
+    #     st.write(f'{num}. {song}')
 
 
